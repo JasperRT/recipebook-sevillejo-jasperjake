638ffa860feec9eda5c4f4463bb2fbc47ff6ea6f
Jasper <jasper.sevillejo@student.ateneo.edu>
2024-03-07 15:54:41 +0800

    Replaced hard-coded data with models-based code.

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        # "Temporary" URL, ingredients are not actually accessible
+        # because the model does not have a corresponding URL in
+        # ledger/urls.py, no view function in ledger/views.py,
+        # and no template in the ledger/templates/ledger folder.
+        return "ingredient/" + self.id + "/" + str(self.name)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("ledger:recipe_detail", args=[self.id, str(self.name)])
+
+
+class RecipeIngredient(models.Model):
+    quantity = models.IntegerField()
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name='recipe')
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='ingredients')
--- ledger/urls.py
+++ ledger/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import recipes_list, recipe_one, recipe_two
+from .views import recipes_list, recipe_detail
 
 urlpatterns = [
-    path('recipes/list', recipes_list, name="recipes-list"),
-    path('recipe/1', recipe_one, name="recipe-one"),
-    path('recipe/2', recipe_two, name="recipe-two")
+    path('recipes/list', recipes_list, name="recipes_list"),
+    path('recipe/<int:id>/<str:name>', recipe_detail, name="recipe_detail"),
 ]
 
 app_name = 'ledger'
